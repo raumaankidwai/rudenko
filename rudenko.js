6bef332b44127886b50f964bfa70333aa78d05c0
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-12-20 11:39:50 -0500

    Update rudenko.js

--- rudenko.js
+++ rudenko.js
@@ -1,34 +1,37 @@
 class RudenkosDisk {
 	constructor () {
-		this.L = [0, 0, 0, 0, 0, 0, 0];
-		this.M = [1, 1, 1, 1, 1, 1, 1];
-		this.R = [0, 0, 0, 0, 0, 0, 0];
+		this.L = [];
+		this.M = [0, 1, 2, 3, 4, 5, 6];
+		this.R = [];
 	}
 	
 	move (move) {
 		var start = move[0];
 		var end = move[1];
 		
-		var color;
+		var startcolor;
 		
 		for (let i = 0; i < 7; i ++) {
-			if (this[start][i]) {
-				color = i;
+			if (typeof this[start][i] == "number") {
+				startcolor = i;
 				break;
 			}
 		}
 		
-		if (!color) {
+		if (!startcolor) {
 			throw new Error(`Rudenko's Disk: Move ${move}: No colors in slice ${start}`);
 		}
 		
-		for (let i = 0; i < color; i ++) {
-			if (this[end][i]) {
-				throw new Error(`Rudenko's Disk: Move ${move}: Blocked`);
+		var endcolor = startcolor;
+		
+		for (let i = 0; i <= startcolor; i ++) {
+			if (typeof this[end][i] == "number") {
+				endcolor = i - 1;
+				break;
 			}
 		}
 		
-		this[start][color] = false;
-		this[end][color] = true;
+		this[end][endcolor] = this[start].slice()[startcolor];
+		this[start][startcolor] = undefined;
 	}
 }
