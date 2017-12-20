8c2ff03dc2925a72f89cfb2f99d52b8ac060de1c
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-12-20 12:59:55 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+const RudenkosDisk = require("./rudenko.js");
+
+var disk = new RudenkosDisk();
+
+for (var i = 0; i < 100; i ++) {
+	(function f () {
+		try {
+			var move = ["LM", "LR", "ML", "MR", "RL", "RM"][Math.floor(Math.random() * 6)];
+			
+			disk.move(move);
+			console.log(move);
+		} catch (e) {
+			f();
+		}
+	});
+}
+
+console.log(disk.L);
+console.log(disk.M);
+console.log(disk.R);
